f28535b13da709707027be2bb1de360e70129fbb
Abdul-Jabbar22 <abduljabbarrar@example.com>
2025-05-17 09:43:37 +0500

    basic structure both front end and backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Employee Management API is running!');
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000; // Fallback to 3000 if PORT not in env
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-     
-          <div>
-            <p className='text-red-500'>Hello with tailwind</p>
+     <Navbar/>
+          <div class="bg-green content-center ">
+            <p className='text-red-500 '>Hello with tailwind</p>
           </div>
     </>
   )
